d88a9ff34cc487fa0a26b4dcec085d861dd4ea12
rich <richard.nurser@gmail.com>
2020-10-05 10:57:06 +0100

    Replace EC2 instance with ASG

--- main.tf
+++ main.tf
@@ -8,10 +8,18 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0ede7f804d699ea83"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0ede7f804d699ea83"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -19,8 +27,24 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuration with an ASG.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key		= "Name"
+    value	= "terraform-asg-example"
+    propagate_at_launch	= true
   }
 }
 
@@ -35,7 +59,3 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP of the server"
-}
